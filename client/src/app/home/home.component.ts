c264396db280863dec3a6e6539b7ef1faefb32cf
adrianomisina <adriano.misina@gmail.com>
2024-07-15 15:51:55 -0300

    feat: criação do componente product

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,20 +1,25 @@
 import { Component } from '@angular/core';
 import { ProductsService } from '../service/products.service';
-import { Products } from '../types';
+import { Product, Products } from '../types';
+import { ProductComponent } from '../components/product/product.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   constructor(private productsService: ProductsService) {}
 
+  products: Product[] = [];
+
   ngOnInit() {
-    this.productsService.getProducts('http://localhost:3000/clothes', {page: 0, perPage: 5}).subscribe((products: Products) => {
-      console.log(products.items);
-    });
+    this.productsService
+      .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
+      .subscribe((products: Products) => {
+        console.log(products.items);
+      });
   }
 }
