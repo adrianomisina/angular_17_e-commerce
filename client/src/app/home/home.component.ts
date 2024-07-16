3f2c3edb1e63f0d685589bc8f088f817424d58c1
adrianomisina <adriano.misina@gmail.com>
2024-07-16 14:39:08 -0300

    feat: método de paginação

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -3,11 +3,12 @@ import { ProductsService } from '../service/products.service';
 import { Product, Products } from '../types';
 import { ProductComponent } from '../components/product/product.component';
 import { CommonModule } from '@angular/common';
+import { PaginatorModule } from 'primeng/paginator';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductComponent, CommonModule],
+  imports: [ProductComponent, CommonModule, PaginatorModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -17,7 +18,16 @@ export class HomeComponent {
   products: Product[] = [];
 
   onProductOutput(product: Product) {
-    console.log(product)
+    console.log(product);
+  }
+
+  fetchProducts(page: number, perPage: number) {
+    this.productsService
+      .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
+      .subscribe((products: Products) => {
+        // console.log(products.items);
+        this.products = products.items;
+      });
   }
 
   ngOnInit() {
@@ -25,7 +35,7 @@ export class HomeComponent {
       .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
       .subscribe((products: Products) => {
         // console.log(products.items);
-        this.products = products.items
+        this.products = products.items;
       });
   }
 }
