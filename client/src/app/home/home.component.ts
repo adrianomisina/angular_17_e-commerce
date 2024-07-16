80eb80efffefc2a6ddaea0a744ec623df0ae6de9
adrianomisina <adriano.misina@gmail.com>
2024-07-15 21:36:45 -0300

    feat: product - input e output

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ProductsService } from '../service/products.service';
 import { Product, Products } from '../types';
 import { ProductComponent } from '../components/product/product.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductComponent],
+  imports: [ProductComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -14,6 +15,10 @@ export class HomeComponent {
   constructor(private productsService: ProductsService) {}
 
   products: Product[] = [];
+
+  onProductOutput(product: Product) {
+    console.log(product)
+  }
 
   ngOnInit() {
     this.productsService
