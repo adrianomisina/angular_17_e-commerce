ca330b745aa751861f3fd6987d02a432dda38db8
adrianomisina <adriano.misina@gmail.com>
2024-08-12 05:36:56 -0300

    feat: construcao dos metodos para add, delete e edit no front

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -32,31 +32,60 @@ export class HomeComponent {
   fetchProducts(page: number, perPage: number) {
     this.productsService
       .getProducts('http://localhost:3000/clothes', { page, perPage })
-      .subscribe((products: Products) => {
-        this.products = products.items;
-        this.totalRecords = products.total;
+      .subscribe({
+        next: (data:Products) => {
+          this.products = data.items;
+          this.totalRecords = data.total
+        }
       });
   }
 
   editProduct(product: Product, id: number) {
     this.productsService.editProduct(`http://localhost:3000/clothes/${id}`, product).subscribe(
       {
-        next: (data) => console.log(data),
+        next: (data) => {
+          console.log(data)
+          this.fetchProducts(0, this.rows);
+        },
         error:(error) => console.log(error),
       }
     )
   }
 
-  deleteProduct(product: Product) {
-    console.log(product, 'Delete')
+  deleteProduct(id: number) {
+    this.productsService.deleteProduct(`http://localhost:3000/clothes/${id}`).subscribe(
+      {
+        next:(data) => {
+          console.log(data);
+          this.fetchProducts(0, this.rows);
+        },
+        
+        error: (error) => {
+          console.log(error)
+        }
+      }
+    )
+
   }
 
   addProduct(product: Product) {
-    console.log(product, 'Add')
+    this.productsService.addProduct(`http://localhost:3000/clothes/`, product).subscribe(
+      {
+        next:(data) => {
+          console.log(data);
+          this.fetchProducts(0, this.rows);
+        },
+        
+        error: (error) => {
+          console.log(error)
+        }
+      }
+    )
+
   }
 
   ngOnInit() {
     this.fetchProducts(0, this.rows);
   }
 }
-// 2:37:05
+// 2:37:52
